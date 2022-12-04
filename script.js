34fce35b1a41381bd634e6c207f3fbce9d09083a
FlorAguirre <floraguirre1598@gmail.com>
2022-12-03 21:46:53 -0500

    Se agregó js para mostrar y ocultar formularios

---
+++ script.js
@@ -0,0 +1,20 @@
+function mostrarFormulario(){ 
+    var formulario = document.getElementById('Formulario');
+    formulario.style.display = "block";  
+  }
+
+function ocultarFormulario(){ 
+    var formulario = document.getElementById('Formulario');
+    formulario.style.display = "none";  
+  }
+
+
+function mostrarFormulario2(){ 
+    var formulario = document.getElementById('Formulario2');
+    formulario.style.display = "block";  
+  }
+
+function ocultarFormulario2(){ 
+    var formulario = document.getElementById('Formulario2');
+    formulario.style.display = "none";  
+  }
